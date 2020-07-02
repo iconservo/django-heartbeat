f2211a780ae9de887cb0aa57a792f4ce7e108fac
Kannan Puthuval <kannan.puthuval@scotts.com>
2020-07-02 15:02:37 -0700

    update author contact info

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     name='django-heartbeat',
     version='0.0.1',
     packages=find_namespace_packages(),
-    url='https://bitbucket.org/redfunction/django-heartbeat',
+    url='https://github.com/iconservo/django-heartbeat/',
     license='MIT',
-    author='Margus Laak',
-    author_email='margus.laak@redfunction.ee',
+    author='IoT Developers @ The Scotts Company',
+    author_email='iot-dev@scotts.com',
     description='A single api endpoint to display django system status',
     install_requires=[
         'django>=2.2,<2.3',
