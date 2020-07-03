6af742097cfb159532f8f21010e2037336948481
kannan-scotts <64853417+kannan-scotts@users.noreply.github.com>
2020-07-02 18:27:03 -0700

    Update package (#6)
    * update author contact info
    
    * update author contact info
    
    * update .gitignore
    
    * fix README.rst syntax from MD to RST

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setup(
     name='django-heartbeat',
     version=django_heartbeat.__version__,
     packages=find_namespace_packages(),
-    url='https://bitbucket.org/redfunction/django-heartbeat',
+    url='https://github.com/iconservo/django-heartbeat/',
     license='MIT',
-    author='Margus Laak',
-    author_email='margus.laak@redfunction.ee',
+    author='IoT Developers @ The Scotts Company',
+    author_email='iot-dev@scotts.com',
     description='A single api endpoint to display django system status',
     install_requires=[
         'django>=2.2,<2.3',
